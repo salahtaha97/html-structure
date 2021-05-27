7fdd2671ee07fb4fdb2e5e13e048e217f494b831
salahtaha <>
2021-05-27 13:58:37 +0000

    add java script to html

---
+++ java script/app.js
@@ -0,0 +1,17 @@
+var yourname = prompt("Whats your name ?");
+alert("welcome Mr."  + yourname)
+var age = prompt("how old are you?")
+var answer = prompt("Do you know what is HTML?")
+
+if(answer === "yes" ){
+  // console.log("what do you know?");
+  alert("graet")
+}else{
+  ("this page will deifne HTML") 
+}
+
+
+
+
+
+
