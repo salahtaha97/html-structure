8e5c5593ad262602c21795b6fa86f58cbcb984b9
salahtaha <>
2021-05-30 14:26:23 +0000

    add new codes

--- java script/app.js
+++ java script/app.js
@@ -2,12 +2,26 @@ var yourname = prompt("Whats your name ?");
 alert("welcome Mr."  + yourname)
 var age = prompt("how old are you?")
 var answer = prompt("Do you know what is HTML?")
-
 if(answer === "yes" ){
-  // console.log("what do you know?");
   alert("graet")
 }else{
-  ("this page will deifne HTML") 
+ alert ("this page will deifne HTML") 
+}
+
+
+while(yourname != 'Salah' || age !=24 ) {
+ console.log('good');
+ username = prompt('Please enter Salah');
+   userAge = prompt('please enter age');
+ break;
+   }
+
+   var x = prompt("How do you rate the article? good or bad?")
+
+  if(x==="good"){
+ document.write('<img src="https://png.pngtree.com/png-vector/20201230/ourlarge/pngtree-five-stars-rating-shiny-golden-like-thumb-png-image_2673053.jpg" />')
+}else{
+ alert("I will be good in next time") 
 }
 
 
